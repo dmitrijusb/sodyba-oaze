3651fc704c586bccc28458f847d81fe58b183058
Dmitrijus <dmitrijus.byckovas@stud.svako.lt>
2022-12-03 20:02:18 +0200

    Added attractions section

--- script/booking.js
+++ script/booking.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const currentDateTime = new Date();
-const year = currentDateTime.getFullYear();
-const month = currentDateTime.getMonth() + 1;
-const date = currentDateTime.getDate() + 1;
+let currentDateTime = new Date();
+let year = currentDateTime.getFullYear();
+let month = currentDateTime.getMonth() + 1;
+let date = currentDateTime.getDate() + 1;
 
 if (date < 10) {
   date = "0" + date;
@@ -12,9 +12,9 @@ if (month < 10) {
   month = "0" + month;
 }
 
-const dateTomorrow = year + "-" + month + "-" + date;
-const checkinElem = document.querySelector("#checkin-date");
-const checkoutElem = document.querySelector("#checkout-date");
+let dateTomorrow = year + "-" + month + "-" + date;
+let checkinElem = document.querySelector("#checkin-date");
+let checkoutElem = document.querySelector("#checkout-date");
 
 checkinElem.setAttribute("min", dateTomorrow);
 
