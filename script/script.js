d5b1ce854c41b1e3067c1d08d9a308be190b153b
Dmitrijus <dmitrijus.byckovas@stud.svako.lt>
2022-12-09 15:26:45 +0200

    Code Clean Up, added favicon, added seo

--- script/script.js
+++ script/script.js
@@ -1,32 +1,52 @@
 "use strict";
 
-// Variables
+//////////////////////
+///Rezervuoti button//
+//////////////////////
+
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".nav-cta");
 
-//Functions
-function closeModal() {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-}
-
-function openModal() {
-  //remove class. NO DOTS
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+function toggleModal() {
+  modal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
 }
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", openModal);
+  btnsOpenModal[i].addEventListener("click", toggleModal);
 }
 
-// Event listeners
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
+btnCloseModal.addEventListener("click", toggleModal);
+overlay.addEventListener("click", toggleModal);
 
-//Keyboard events - global events
 document.addEventListener("keydown", function (e) {
-  if (!modal.classList.contains("hidden") && e.key === "Escape") closeModal();
+  if (!modal.classList.contains("hidden") && e.key === "Escape") toggleModal();
 });
+
+//////////////////////
+///Form Date Format//
+//////////////////////
+
+let currentDateTime = new Date();
+let year = currentDateTime.getFullYear();
+let month = currentDateTime.getMonth() + 1;
+let date = currentDateTime.getDate() + 1;
+
+if (date < 10) {
+  date = "0" + date;
+}
+if (month < 10) {
+  month = "0" + month;
+}
+
+let dateTomorrow = year + "-" + month + "-" + date;
+let checkinElem = document.querySelector("#checkin-date");
+let checkoutElem = document.querySelector("#checkout-date");
+
+checkinElem.setAttribute("min", dateTomorrow);
+
+checkinElem.onchange = function () {
+  checkoutElem.setAttribute("min", this.value);
+};
